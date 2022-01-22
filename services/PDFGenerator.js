f73347736124f9bd778870cc1aaed5eea7b4f190
betuah <betuahanugerah@gmail.com>
2022-01-22 12:35:19 +0700

    Dynamic template

--- services/PDFGenerator.js
+++ services/PDFGenerator.js
@@ -1,68 +1,89 @@
 const PDFDocument   = require('pdfkit')
 const QRCode        = require('qrcode')
+const axios         = require('axios')
+const doc = require('pdfkit')
 
 const generate = async (data, dataCallback, endCallback) => {
-    const name            = data.name
-    const date            = data.date
-    const title           = data.title
-    const participantId   = data.participantId
-    const certificateId   = data.number
-    const signatureDate   = data.signatureDate
-    const backCertificate = data.backCertificate
-    const frontCertificate = data.frontCertificate
-    const fontCollor      = data.fontCollor
-
-    const capitalizeName = name.split(" ")
-
-    const fullName = capitalizeName.map((word) => { 
-        return word[0].toUpperCase() + word.substring(1)
-    }).join(" ")
-
-    const qrOpts = {
-        color: {
-            dark:"#232323",
-        }
-    }
-
-    let signature   = await QRCode.toDataURL(`https://technobrainlab.com/certificate/signature/${signatureDate}`, qrOpts)
-    let code        = await QRCode.toDataURL(`https://technobrainlab.com/certificate/${participantId}`, qrOpts)
-
     const doc = new PDFDocument({
         size: 'A4',
         layout: 'landscape',
         info: {
-            Title: `${title} - ${fullName}`,
+            Title: `${data.courseTitle} - ${data.fullName}`,
         },
         margins: { top: 0, left: 0, right: 0, bottom: 0 }
     })
 
     doc.on('data', dataCallback)
     doc.on('end', endCallback)
-    doc.image(`public/images/sertifikat/${frontCertificate}`, 0, 0, { width: 842})
-        .image('public/images/technobrain-logo.png', 320, 45, { width: 120})
-        .image('public/images/aws-logo.png', 460, 45, { width: 60})
-        .fillColor(`${fontCollor}`).font('public/fonts/Montserrat/Montserrat-Bold.ttf').fontSize(35).text(fullName, 0, 250, {
-            align: 'center'
-        })
-        .fillColor(`${fontCollor}`).font('public/fonts/Montserrat/Montserrat-SemiBold.ttf').fontSize(14).text(title.toUpperCase(), 0, 350, {
-            align: 'center'
-        })
-        .fillColor(`${fontCollor}`).font('public/fonts/Montserrat/Montserrat-SemiBold.ttf').fontSize(12).text(date, 0, 413, {
-            align: 'center'
-        })
-        .image(signature, 390, 435, { width: 60, align: 'center' })
 
-    doc.addPage({
-        size: 'A4',
-        layout: 'landscape'
-    })
-    doc.image(`public/images/sertifikat/${backCertificate}`, 0, 0, { width: 842})
-    doc.image(code, 385, 418, { width: 80, align: 'center' })
-    doc.font('public/fonts/Montserrat/Montserrat-Regular.ttf').fontSize(8).text(certificateId, 78, 495, {
-        align: 'center'
-    })
+    try {
+        const name            = data.fullName
+        const participantId   = data.participantId
+        const certificateId   = data.certificateNumber
+        const template        = data.template
+        const frontImage      = (await axios.get(`${template.front}`, { responseType: 'arraybuffer' })).data
+        const front           = `data:image/jpg;base64,${Buffer.from(frontImage).toString('base64')}`
+        const backImage       = (await axios.get(`${template.back}`, { responseType: 'arraybuffer' })).data
+        const back            = `data:image/jpg;base64,${Buffer.from(backImage).toString('base64')}`
 
-    doc.end()
+        const capitalizeName = name.split(" ")
+        const fullName = capitalizeName.map((word) => { 
+            if (word.length > 1) {
+                return word[0].toUpperCase() + word.substring(1)
+            } else {
+                return word[0].toUpperCase()
+            }
+        }).join(" ")
+
+        const qrOpts = {
+            color: {
+                dark:"#232323",
+            }
+        }
+
+        // let signature   = await QRCode.toDataURL(`https://technobrainlab.com/certificate/signature/${signatureDate}`, qrOpts)
+        let code        = await QRCode.toDataURL(`https://technobrainlab.com/certificate/${participantId}`, qrOpts)
+
+        doc.image(`${front}`, 0, 0, { width: 842})
+        doc.fillColor(`${template.fontColor}`)
+            .font('public/fonts/Montserrat/Montserrat-Bold.ttf')
+            .fontSize(template.name.fontSize)
+            .text(fullName, template.name.x, template.name.y, {
+                align: `${template.name.align}`
+            })
+
+        doc.addPage({
+            size: 'A4',
+            layout: 'landscape'
+        })
+        doc.image(`${back}`, 0, 0, { width: 842})
+        
+        if (template.qrcode) {
+            doc.image(code, template.qrcode.x, template.qrcode.y, { width: template.qrcode.size, align: `${template.qrcode.align}` })
+        }
+
+        if (template.certificateNumber) {
+            doc.font('public/fonts/Montserrat/Montserrat-Regular.ttf')
+                .fontSize(template.certificateNumber.fontSize)
+                .text(certificateId, template.certificateNumber.x, template.certificateNumber.y, {
+                    align: 'center'
+                })
+        }
+
+        doc.end()
+    } catch (error) {
+        console.log(error)
+        doc.end()
+    }
+
+    
+}
+
+const imageEncode = (arrayBuffer) => {
+    let u8 = new Uint8Array(arrayBuffer)
+    let b64encoded = btoa([].reduce.call(new Uint8Array(arrayBuffer),function(p,c){return p+String.fromCharCode(c)},''))
+    let mimetype="image/jpeg"
+    return "data:"+mimetype+";base64,"+b64encoded
 }
 
 module.exports = { generate }
