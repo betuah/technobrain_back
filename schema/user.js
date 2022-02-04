1c94fd6016a81b514063a540d3e6821f2e8cdd18
betuah <betuahanugerah@gmail.com>
2022-02-04 13:56:33 +0700

    add user schema:

---
+++ schema/user.js
@@ -0,0 +1,23 @@
+const { Validator } = require('express-json-validator-middleware')
+const { validate }  = new Validator()
+
+const signUpSchema = {
+    type: "object",
+    required: ["fullName", "email", "password"],
+    properties: {
+        fullName: {
+            type: "string",
+        },
+        email: {
+            type: "string",
+            pattern: "^\\S+@\\S+\\.\\S+$"
+        },
+        password: {
+            type: "string",
+        }
+    },
+}
+
+const signUp = validate({ body: signUpSchema })
+
+module.exports = { signUp }
